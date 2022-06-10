dc866898ad57c7def9ac36539104c996517de661
AdityaP <blackholesarereal@gmail.com>
2022-06-10 14:01:32 +0530

    first commit02

--- app/src/main/java/com/example/jobportal/MainActivity.java
+++ app/src/main/java/com/example/jobportal/MainActivity.java
@@ -2,15 +2,52 @@ package com.example.jobportal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button read,menu;
+    TextView quote;
+    TextToSpeech textToSpeech;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        read = findViewById(R.id.b1);
+        menu = findViewById(R.id.menu);
+
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if(i == textToSpeech.SUCCESS){
+                    int lang = textToSpeech.setLanguage(Locale.ENGLISH);
+                }
+            }
+        });
+        read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = "You're Opportunity awaits you";
+                int speech = textToSpeech.speak(s,textToSpeech.QUEUE_FLUSH,null);
+            }
+        });
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Menu.class);
+                startActivity(intent);
+            }
+        });
 
         MediaPlayer music = MediaPlayer.create(MainActivity.this, R.raw.jbpp);
         music.start();
